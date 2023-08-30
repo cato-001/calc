58337694455cb0ad21cc1c0fb272182ab656d00d
lukas <lukas.weber.4.12@gmail.com>
2023-08-30 20:20:46 +0200

    add add expression

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 fn main() {
-    let expression = Expression::Number(73);
+    let expression = Expression::Add((Expression::Number(73), Expression::Number(12312)).into());
     let result = expression.evaluate();
     println!("{}", result)
 }
 
 enum Expression {
     Number(i32),
+    Add(Box<(Self, Self)>),
 }
 
 impl Expression {
     fn evaluate(&self) -> i32 {
         match self {
             Expression::Number(value) => *value,
+            Expression::Add(values) => {
+                let (value, other) = values.as_ref();
+                value.evaluate() + other.evaluate()
+            }
         }
     }
 }
