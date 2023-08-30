41aa40382c371a8655de365f60dcdf23f056e8a5
lukas <lukas.weber.4.12@gmail.com>
2023-08-30 20:11:30 +0200

    add expression

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let expression = Expression::Number(73);
+    let result = expression.evaluate();
+    println!("{}", result)
+}
+
+enum Expression {
+    Number(i32),
+}
+
+impl Expression {
+    fn evaluate(&self) -> i32 {
+        match self {
+            Expression::Number(value) => *value,
+        }
+    }
 }
