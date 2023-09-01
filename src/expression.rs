63aa4b56358a0183457cfc1be13d10aeedb8dfb7
Lukas412 <lukas.weber.4.12@gmail.com>
2023-09-01 10:15:33 +0200

    implement subtraction

--- src/expression.rs
+++ src/expression.rs
@@ -1,50 +1,51 @@
-use nom::{Finish, IResult};
 use nom::branch::alt;
 use nom::combinator::complete;
+use nom::{Finish, IResult};
 
 use crate::error::SyntaxError;
-use crate::expression::add::Add;
+use crate::expression::add::AddAndSub;
 use crate::expression::negative::Negative;
 use crate::expression::number::Number;
 
-mod number;
-mod negative;
 mod add;
+mod negative;
+mod number;
 mod symbol;
 
 pub enum Expression {
-    Number(Number),
-    Negative(Negative),
-    Add(Add),
+  Number(Number),
+  Negative(Negative),
+  AddAndSub(AddAndSub),
 }
 
 impl Expression {
-    pub fn parse(input: &str) -> Result<Self, SyntaxError> {
-        Self::parser(input)
-            .finish()
-            .map_err(Into::into)
-            .and_then(|result| if result.0.is_empty() {
-                Ok(result.1)
-            } else {
-                Err(SyntaxError::new(format!("ExpressionParsingIncomplete: {}", result.0)))
-            })
-    }
-
-    fn parser(input: &str) -> IResult<&str, Self> {
-        complete(alt((
-            Add::parser,
-            Negative::parser,
-            Number::parser,
-        )))(input)
-    }
-}
-
-impl Expression {
-    pub fn evaluate(&self) -> i32 {
-        match self {
-            Self::Number(expression) => expression.evaluate(),
-            Self::Negative(expression) => expression.evaluate(),
-            Self::Add(expression) => expression.evaluate(),
+  pub fn parse(input: &str) -> Result<Self, SyntaxError> {
+    Self::parser(input)
+      .finish()
+      .map_err(Into::into)
+      .and_then(|result| {
+        if result.0.is_empty() {
+          Ok(result.1)
+        } else {
+          Err(SyntaxError::new(format!(
+            "ExpressionParsingIncomplete: {}",
+            result.0
+          )))
         }
+      })
+  }
+
+  fn parser(input: &str) -> IResult<&str, Self> {
+    complete(alt((AddAndSub::parser, Negative::parser, Number::parser)))(input)
+  }
+}
+
+impl Expression {
+  pub fn evaluate(&self) -> i32 {
+    match self {
+      Self::Number(expression) => expression.evaluate(),
+      Self::Negative(expression) => expression.evaluate(),
+      Self::AddAndSub(expression) => expression.evaluate(),
     }
+  }
 }
--- src/expression/add.rs
+++ src/expression/add.rs
@@ -1,26 +1,68 @@
 use nom::branch::alt;
-use nom::multi::separated_list1;
+use nom::character::complete::one_of;
+use nom::multi::many1;
 use nom::IResult;
 
 use crate::expression::negative::Negative;
 use crate::expression::number::Number;
-use crate::expression::symbol::char_with_whitespace;
 use crate::expression::Expression;
 
-pub struct Add(Vec<Expression>);
+pub struct AddAndSub {
+  start: Box<Expression>,
+  parts: Vec<(Operator, Expression)>,
+}
 
-impl Add {
+impl AddAndSub {
   pub fn parser(input: &str) -> IResult<&str, Expression> {
-    let (input, expressions) =
-      separated_list1(char_with_whitespace('+'), Self::inner_expression)(input)?;
-    Ok((input, Expression::Add(Self(expressions))))
-  }
-
-  fn inner_expression(input: &str) -> IResult<&str, Expression> {
-    alt((Number::parser, Negative::parser))(input)
+    let (input, start) = inner_expression(input)?;
+    let (input, parts) = many1(inner_expression_with_operator)(input)?;
+    Ok((
+      input,
+      Expression::AddAndSub(Self {
+        start: start.into(),
+        parts,
+      }),
+    ))
   }
 
   pub fn evaluate(&self) -> i32 {
-    self.0.iter().map(Expression::evaluate).sum()
+    self
+      .parts
+      .iter()
+      .fold(self.start.evaluate(), |sum, current| {
+        let (operator, expression) = current;
+        let result = expression.evaluate();
+        match operator {
+          Operator::Add => sum + result,
+          Operator::Sub => sum - result,
+        }
+      })
   }
+}
+
+enum Operator {
+  Add,
+  Sub,
+}
+
+impl Operator {
+  fn parser(input: &str) -> IResult<&str, Operator> {
+    let (input, operator) = one_of("+-")(input)?;
+    let operator = match operator {
+      '+' => Self::Add,
+      '-' => Self::Sub,
+      _ => unreachable!(),
+    };
+    Ok((input, operator))
+  }
+}
+
+fn inner_expression_with_operator(input: &str) -> IResult<&str, (Operator, Expression)> {
+  let (input, operator) = Operator::parser(input)?;
+  let (input, expression) = inner_expression(input)?;
+  Ok((input, (operator, expression)))
+}
+
+fn inner_expression(input: &str) -> IResult<&str, Expression> {
+  alt((Number::parser, Negative::parser))(input)
 }
