d0ccd39bf1b88c76f300e1eb654aeab390aadc1c
Lukas412 <lukas.weber.4.12@gmail.com>
2023-09-01 11:32:55 +0200

    reformat files

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use std::env;
 use crate::expression::Expression;
+use std::env;
 
-mod expression;
 mod error;
+mod expression;
 
 fn main() {
-    let results = env::args().skip(1).map(|arg| {
-        Expression::parse(&arg).map(|expression| expression.evaluate())
-    });
-    for result in results {
-        match result {
-            Ok(value) => println!("{}", value),
-            Err(error) => println!("SyntaxError({})", error)
-        }
+  let results = env::args()
+    .skip(1)
+    .map(|arg| Expression::parse(&arg).map(|expression| expression.evaluate()));
+  for result in results {
+    match result {
+      Ok(value) => println!("{}", value),
+      Err(error) => println!("SyntaxError({})", error),
     }
+  }
 }
